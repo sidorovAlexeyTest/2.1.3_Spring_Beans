47b4f3aa5282740b40000398b5319921b19363b7
Sidorov Aleksey <cobetckoeyxo@mail.ru>
2021-10-02 13:26:58 +1000

    2.1.3 Spring Beans (@Autowired, @Qualifire)

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import org.springframework.stereotype.Component;
+
+@Component("dog")
+public class Dog extends Animal{
+
+    @Override
+    public String toString() {
+        return "I`m dog";
+    }
+}
